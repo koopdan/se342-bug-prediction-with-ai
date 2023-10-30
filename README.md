9f988b2cd915d234a7e255c5ade32be39dd4de7f
Faizi Fifita <94952795+FayZ676@users.noreply.github.com>
2023-10-30 15:02:36 -0700

    test scraping llm chat dashboards.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 ## Chromedriver Download
 - <https://chromedriver.chromium.org/>
+**For Apple Devices:** You may get an error when first running that says "Error: “chromedriver” cannot be opened because the developer cannot be verified. Unable to launch the chrome browser". Run `xattr -d com.apple.quarantine <path_to_the_chromedriver>` to grant access.
--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 import gradio as gr
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
-def sketch_recognition(img):
-    pass  # Implement your sketch recognition model here...
+from dotenv import load_dotenv
+import os
 
 
-gr.Interface(fn=sketch_recognition, inputs="sketchpad", outputs="label").launch()
+def initialize_driver(chromedriver_path: str):
+    """Initializes and returns a WebDriver instance."""
+    chrome_options = ChromeOptions()
+    # chrome_options.add_argument("--headless")  # Run in headless mode (no GUI). Comment out to see what's going on.
+    chrome_service = ChromeService(chromedriver_path)
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return driver
+
+
+def main():
+    load_dotenv()
+    chromedriver_path = os.getenv("CHROMEDRIVER_PATH")
+
+    chatgpt_url = "https://chat.openai.com/?model=text-davinci-002-render-sha"
+    bard_url = "https://bard.google.com/chat?utm_source=sem&utm_medium=paid-media&utm_campaign=q4enUS_sem7"
+    claude_url = "https://claude.ai/chats"
+    urls_list = [chatgpt_url, bard_url, claude_url]
+
+    driver = initialize_driver(chromedriver_path)
+    for url in urls_list:
+        driver.get(url)
+
+    # Initialize WebDriver
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
